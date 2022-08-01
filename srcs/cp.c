7d954bcc64b1aa8284a7e03ebebc2782ecd4511e
harinez2 <yonishi@student.42tokyo.jp>
2022-08-02 00:01:20 +0900

    refactored check_puttable() logic

--- srcs/cp.c
+++ srcs/cp.c
@@ -1,15 +1,14 @@
 #include "cp.h"
 
-bool check_puttable(t_shape shape, const char Table[TABLE_ROW][TABLE_COL]){
-	char **array = shape.table_shape;
-	for(int i = 0; i < shape.side_length;i++) {
-		for(int j = 0; j < shape.side_length ;j++){
-			if((shape.col+j < 0 || shape.col+j >= TABLE_COL || shape.row+i >= TABLE_ROW)){
-				if(array[i][j])
-					return false;
-				
-			}
-			else if(Table[shape.row+i][shape.col+j] && array[i][j])
+bool check_puttable(const t_shape shape, const char table[TABLE_ROW][TABLE_COL]) {
+	for (int i = 0; i < shape.side_length; i++) {
+		for (int j = 0; j < shape.side_length; j++){
+			if (!shape.table_shape[i][j])
+				continue ;
+			if (shape.col + j < 0
+				|| shape.col + j >= TABLE_COL
+				|| shape.row + i >= TABLE_ROW
+				|| table[shape.row + i][shape.col + j])
 				return false;
 		}
 	}
